562827c35f59d45f14e5badead720d2d67c30320
Aariyan7 <aariyanveera2k6@gmail.com>
2025-05-30 14:45:49 +0530

    Made some changes in user.py

--- USER/user.py
+++ USER/user.py
@@ -35,19 +35,24 @@ class User:
 
                 self.cursor.execute("SELECT user_id, user_name, user_password FROM user WHERE user_name = %s AND user_password = %s", (user_name, user_password))
                 row = self.cursor.fetchone()
-                print(row)
-                login_userid = row[0]
-                login_username = row[1]
-                login_userpassword = row[2]
 
-                if user_name == login_username and user_password == login_userpassword:
-                    print("Logged in sucessfully")
-                    self.is_logedin = True
-                    self.user_id = login_userid
+                if row is not None:
+                    login_userid = row[0]
+                    login_username = row[1]
+                    login_userpassword = row[2]
+
+                    if user_name == login_username and user_password == login_userpassword:
+                        print("Logged in sucessfully")
+                        self.is_logedin = True
+                        self.user_id = login_userid
+
+                    else:
+                        print("User name or Password is wrong")
 
                 else:
-                    print("User does not exists")
+                     print("User does not exists")
 
+                
             else:
                 print("Data Base has been disconnected for some reason")
 
